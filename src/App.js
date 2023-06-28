b91e87767fa06a89d0b1f406544c0141241d1d8e
Christoffer Rydeståhl <kristoffer@oholics.com>
2023-06-28 15:18:10 +0200

    Added bus tickets.

--- src/App.js
+++ src/App.js
@@ -9,27 +9,53 @@ function App() {
   const [inputData, setInputData] = useState('HHV0ZC');
   const [loading, setLoading] = useState(false); // state variable for loading status
   const [apiKey, setApiKey] = useState('HHV0ZC');
+  const [lastWorkingKey, setLastWorkingKey] = useState('');
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [notToggled, setNotToggled] = useState(false); // state variable
 
   useEffect(() => {
     const fetchData = () => {
       localStorage.clear();
       setLoading(true);
-
+      setLastWorkingKey(apiKey);
       fetch(
         `https://proxyserversalestracker.onrender.com/https://manager.tickster.com/Statistics/SalesTracker/Api.ashx?keys=${apiKey.trim()}`
       )
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
           setData(data);
           // Spara data i local storage -
-          localStorage.setItem('cachedData', JSON.stringify(data));
-          setLoading(false);
+          if (data[0].ven.vrc === 'AMEP2ZTG94GUJNV') {
+            localStorage.setItem('cachedData', JSON.stringify(data));
+            setApiKey(inputData);
+            setLastWorkingKey(apiKey);
+            setLoading(false);
+          }
         })
         .catch((error) => console.error(error));
     };
     fetchData();
   }, [apiKey]);
+
+  const toggleInfoFunction = (index) => {
+    // function to toggle the active index
+    notToggled ? setNotToggled(false) : setNotToggled(true);
+    if (activeIndex === index) {
+      // if the current index is already active, set it to null
+      setActiveIndex(null);
+    } else {
+      // otherwise, set it to the current index
+      setActiveIndex(index);
+    }
+  };
+
+  function formatTime(timeStr) {
+    const date = new Date(timeStr);
+    const isoString = date.toISOString();
+    const formattedDate = isoString.substring(0, 10);
+    const formattedTime = isoString.substring(11, 16);
+    return formattedDate + ' ' + formattedTime;
+  }
 
   const handleFocus = () => {
     setInputData('');
@@ -47,43 +73,38 @@ function App() {
   };
 
   const refresh = () => {
-    window.location.reload();
+    setApiKey(lastWorkingKey);
   };
 
   const saveInput = () => {
     setData('');
 
     if (inputData.length === 6) {
-    localStorage.clear();
-    setLoading(true);
-    
-    fetch(
-      `https://proxyserversalestracker.onrender.com/https://manager.tickster.com/Statistics/SalesTracker/Api.ashx?keys=${apiKey.trim()}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        // Spara data i local storage -
-        localStorage.setItem('cachedData', JSON.stringify(data));
-        setApiKey(inputData);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      localStorage.clear();
+      setLoading(true);
+      setLastWorkingKey(apiKey);
+
+      fetch(
+        `https://proxyserversalestracker.onrender.com/https://manager.tickster.com/Statistics/SalesTracker/Api.ashx?keys=${apiKey.trim()}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+          // Spara data i local storage -
+          localStorage.setItem('cachedData', JSON.stringify(data));
+          setApiKey(inputData);
+          setLastWorkingKey(apiKey);
+          localStorage.setItem('lastWorkingKey', JSON.stringify(apiKey));
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     } else {
-      alert('Felaktig nyckel, försök igen.')
+      alert('Felaktig nyckel, försök igen.');
       window.location.reload();
     }
   };
-
-  function formatTime(timeStr) {
-    const date = new Date(timeStr);
-    const isoString = date.toISOString();
-    const formattedDate = isoString.substring(0, 10);
-    const formattedTime = isoString.substring(11, 16);
-    return formattedDate + ' ' + formattedTime;
-  }
 
   if (!data) {
     return (
@@ -91,13 +112,11 @@ function App() {
         <img src={logo} alt='Dalhalla' className='logo' />
         {loading ? (
           <div>
-          <p className='loading'>
-            Laddar data
-          </p>
-          <p className='loadingText'>
-            Detta kan ta lite tid om det är första gången du hämtar
-            data på denna nyckeln.
-          </p>
+            <p className='loading'>Laddar data</p>
+            <p className='loadingText'>
+              Detta kan ta lite tid om det är första gången du hämtar data på
+              denna nyckeln.
+            </p>
           </div>
         ) : (
           <p></p>
@@ -130,13 +149,7 @@ function App() {
       <header className='App-header'>
         <div className='eventFeed'>
           <img src={logo} alt='Dalhalla' className='logo' />
-          {loading ? (
-            <p className='loading'>
-              Laddar data
-            </p>
-          ) : (
-            <p></p>
-          )}
+          {loading ? <p className='loading'>Laddar data</p> : <p></p>}
           {!loading && data[0]?.ven?.vrc === 'AMEP2ZTG94GUJNV' ? (
             data.map((item, index) => (
               <Fade>
@@ -157,30 +170,58 @@ function App() {
                       {item.sales.soldQtyNet + item.sales.freeTktQtyNet}
                     </p>
                     <img src={item.img.thumb} alt={item.name} />
-                    <div className='scannedTickets'></div>
+                    <button onClick={() => toggleInfoFunction(item.erc)}>
+                      {notToggled ? 'Mindre info' : 'Mer info'}
+                    </button>
+                    <div
+                      className='ticketInfo'
+                      style={{
+                        display: activeIndex === item.erc ? 'block' : 'none',
+                      }}
+                    >
+                      <h4>Bussbiljetter</h4>
+                      <br></br>
+
+                      {item.gfs
+                        .filter(
+                          (ticket) =>
+                            ticket.type === 5 &&
+                            ticket.name !== 'VIP Acess Bakre Loge' &&
+                            ticket.name !== 'Avbeställningsskydd' &&
+                            ticket.name !== '3-Rätters meny - Konsertmiddag' &&
+                            ticket.name !== 'VIP Acess Främre Loge' &&
+                            ticket.name !== 'VIP Access'
+                        )
+                        .map((ticket) => (
+                          <div className='ticketInfo'>
+                            <h4>
+                              <b>{ticket.name + ':'}</b>
+                            </h4>
+                            <h4>
+                              Sålt antal:{' '}
+                              <b>{ticket.soldQtyNet + ticket.freeTktQtyNet} </b>
+                            </h4>
+                            <br></br>
+                          </div>
+                        ))}
+                    </div>
+
+                    {notToggled && (
+                      <button
+                        onClick={() => toggleInfoFunction(item.erc)}
+                      >{notToggled ? 'Mindre info' : 'Mer info'}</button>
+                    )}
                   </div>
                 </div>
               </Fade>
             ))
-          ) :   
-              <p className='error'>Nyckeln tillhör inte Dalhalla</p> 
-          }
-          <button className="refresh" onClick={refresh}>Ladda om sidan</button>
+          ) : (
+            <p className='error'>Nyckeln tillhör inte Dalhalla</p>
+          )}
+          <button className='refresh' onClick={refresh}>
+            Ladda om sidan
+          </button>
         </div>
-        
-        
-        <div className='keyInput'>
-          <p className="currentKey">Ladda data från en annan nyckel:</p>
-          <input
-            type='text'
-            value={inputData}
-            onChange={handleInput}
-            placeholder='T ex 12345'
-            onKeyDown={handleKeyDown}
-            onFocus={handleFocus}
-          />
-          <button onClick={saveInput}>Hämta</button>
-        </div>        
       </header>
     </div>
   );
